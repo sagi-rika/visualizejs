8b9f8c5ebc45b325361a30a426250e0ce1372be4
sagir@rapyd.net <sagir@rapyd.net>
2019-11-24 18:45:51 +0200

    Added beautifier

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -2,40 +2,50 @@ import React from 'react';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
-
-import AceEditor from 'react-ace';
-import * as actions from '../store/actions';
-
+// eslint-disable-next-line no-unused-vars
+import _ from 'ace-builds';
+import 'ace-builds/webpack-resolver';
 import 'ace-builds/src-min-noconflict/mode-javascript';
 import 'ace-builds/src-min-noconflict/snippets/javascript';
 import 'ace-builds/src-min-noconflict/theme-tomorrow_night_bright';
 import 'ace-builds/src-min-noconflict/ext-language_tools';
+import 'ace-builds/src-min-noconflict/ext-beautify';
+import AceEditor from 'react-ace';
+import Beautify from 'js-beautify';
+
+import * as actions from '../store/actions';
 
 class Editor extends React.Component {
   shouldComponentUpdate(nextProps) {
     const { code } = this.props;
     if (code !== nextProps.code) {
-      return false;
+      return true;
     }
-    return true;
+    return false;
   }
 
   onChange = newValue => {
     const { updateCode } = this.props;
-    updateCode(newValue);
+    updateCode(
+      Beautify(newValue, {
+        indent_size: 2,
+        space_after_anon_function: true,
+        space_after_named_function: true
+      })
+    );
   };
 
   render() {
+    const { code } = this.props;
+
     return (
       <EditorWrapper>
         <AceEditor
           mode="javascript"
+          value={code}
           theme="tomorrow_night_bright"
           onChange={this.onChange}
           name="UNIQUE_ID_OF_DIV"
-          editorProps={{
-            $blockScrolling: true
-          }}
           fontSize={15}
           tabSize={2}
           enableBasicAutocompletion
@@ -45,8 +55,13 @@ class Editor extends React.Component {
             width: '100%'
           }}
           setOptions={{
-            copyWithEmptySelection: true
+            copyWithEmptySelection: true,
+            fontFamily: '"Fira code", "Fira Mono", monospace',
+            printMargin: 80,
+            showPrintMargin: false
           }}
+          debounceChangePeriod={1000}
+          onLoad={this.onLoad}
         />
       </EditorWrapper>
     );
